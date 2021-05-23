ae352624b2fe0a86d2c4e221a659f2b6386b6d47
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2021-05-23 09:56:44 +0300

    add 2 page

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
 /****************************/
-const headerMenuBtn = document.querySelector('.header-menu');
-const menuMobile = document.querySelector('.menu-mobile');
-
-headerMenuBtn.addEventListener('click', function () {
-  this.classList.toggle("active");
-  menuMobile.classList.toggle("is-show");
-});
+// const headerMenuBtn = document.querySelector('.header-menu');
+// const menuMobile = document.querySelector('.menu-mobile');
+//
+// headerMenuBtn.addEventListener('click', function () {
+//   this.classList.toggle("active");
+//   menuMobile.classList.toggle("is-show");
+// });
 /****************************/
 
-const resultsSlider = new Swiper('.js-slider', {
+const shopssSlider = new Swiper('.shops-list', {
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 1,
+  slidesPerView: 4,
   autoHeight: true,
 
   pagination: {
@@ -38,6 +38,78 @@ const resultsSlider = new Swiper('.js-slider', {
     }
   }
 });
+
+// const shopsPopupSlider = new Swiper('.product-popup-shops', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 3,
+//   autoHeight: true,
+//
+//   pagination: {
+//     el: '.swiper-pagination',
+//     dynamicBullets: true,
+//   },
+//
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1
+//     },
+//     730: {
+//       slidesPerView: 2,
+//       spaceBetween: 30
+//     },
+//     1200: {
+//       slidesPerView: 4,
+//       spaceBetween: 40
+//     }
+//   }
+// });
+
+const products = new Swiper(".beverages-card-products", {
+  // loop: true,
+  spaceBetween: 10,
+  slidesPerView: 7,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+const slider = new Swiper(".js-beverages-slider", {
+  loop: true,
+  spaceBetween: 10,
+  // effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: products,
+  },
+});
+
+// var swiper = new Swiper(".mySwiper", {
+//   loop: true,
+//   spaceBetween: 10,
+//   slidesPerView: 4,
+//   freeMode: true,
+//   watchSlidesVisibility: true,
+//   watchSlidesProgress: true,
+// });
+// var swiper2 = new Swiper(".mySwiper2", {
+//   loop: true,
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: swiper,
+//   },
+// });
 
 
 
@@ -68,6 +140,18 @@ const documentWidth = (document.documentElement.clientWidth); // ширина м
 //   }
 // }
 
+$(function(){
+  $(".product-beverage-list__link").on("click", function(e){
+    e.preventDefault();
+    $(".product-popup-layout").toggleClass("is-open");
+  })
+
+  $(".product-popup-close").on("click", function(e){
+    e.preventDefault();
+    console.log("hello")
+    $(".product-popup-layout").toggleClass("is-open");
+  })
+});
 
 
 /****************************************************************/
